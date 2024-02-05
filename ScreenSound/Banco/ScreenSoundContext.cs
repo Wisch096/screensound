cc25ce0e42d5671ac72089643c330bda7b226783
Matheus Wisch <wisch.pg@outlook.com>
2024-02-05 19:23:35 -0300

    Initial commit

---
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Banco
+{
+    internal class ScreenSoundContext : DbContext
+    {
+        public DbSet<Artista> Artistas { get; set; }
+
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+      
+    }
+
+
+}
